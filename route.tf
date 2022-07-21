ac7868ce311c2386a92a70669027d7c3d35e52ed
Monika Alluru <sree.alluru@cloudreach.com>
2022-07-21 14:47:29 +0530

    route table added1

--- route.tf
+++ route.tf
@@ -31,7 +31,7 @@ resource "aws_route_table_association" "private" {
 }
 
 resource "aws_route_table_association" "data" {
-  subnet_id      = aws_subnet.data
+  subnet_id      = aws_subnet.data.id
   route_table_id = aws_route_table.nat_route_tbl.id
 }
 
